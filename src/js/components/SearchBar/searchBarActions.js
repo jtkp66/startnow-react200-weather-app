76b5c75bf6637c10c78c5a35b1285ec80b0d55c3
Jamie Pham <jtkp66@gmail.com>
2018-07-05 18:55:22 -0700

    capItiLiZation

--- src/js/components/SearchBar/searchBarActions.js
+++ src/js/components/SearchBar/searchBarActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = '34e93ebb37fd183cc885ec35940e2fc3';
 //const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
 //api.openweathermap.org/data/2.5/weather?q={city name}
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';//extract the variable and export it 
